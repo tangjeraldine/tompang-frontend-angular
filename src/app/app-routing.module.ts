586956c8f8a4d29dd9a8aba305bf50388e9713fa
stee-codes <stee.codes@gmail.com>
2023-01-18 10:57:42 +0800

    Front Landing Page
    Added animations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { TripComponent } from './trip/trip.component';
 import { RegisterComponent } from './register/register.component';
-import { UsersTripListComponent } from './users-trip-list/users-trip-list.component';
 import { PublicLandingPgComponent } from './public-landing-pg/public-landing-pg.component';
 import { UsersLandingPgComponent } from './users-landing-pg/users-landing-pg.component';
 import { EmailVerificationPgComponent } from './email-verification-pg/email-verification-pg.component';
+import { UsersTripListComponent } from './users-trip-list/users-trip-list.component';
 import { DiscussionRoomComponent } from './discussion-room/discussion-room.component';
 
 const routes: Routes = [
