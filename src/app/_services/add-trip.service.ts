b6e1c94b73a2e8553dbbd0f188c65ad12b60756a
eugenelye <eugenelye91@gmail.com>
2023-01-18 13:54:27 +0800

    addtripService and addTrip component added

---
+++ src/app/_services/add-trip.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddTripService {
+
+  private baseURL="http://localhost:8080/trips"
+
+  constructor(private authService: AuthenticationService, private httpClient:HttpClient) { }
+
+  addTrip(trip:any) {
+    var auth_params=this.authService.generateAuthHeaders();
+    const customerId=auth_params['header_id']
+
+    return this.httpClient.post(`${this.baseURL}/add/${customerId}`,trip);
+
+
+
+
+  }
+
+}
