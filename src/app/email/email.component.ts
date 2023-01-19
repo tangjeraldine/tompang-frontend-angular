e606b5d8c77e83c0fd5ce85e3dffcf3b2da08d49
eugenelye <eugenelye91@gmail.com>
2023-01-19 15:33:08 +0800

    email service and component updates

--- src/app/email/email.component.ts
+++ src/app/email/email.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CustomerService } from '../_services/customer.service';
 
 @Component({
   selector: 'app-email',
   templateUrl: './email.component.html',
   styleUrls: ['./email.component.css']
 })
-export class EmailComponent {
+export class EmailComponent implements OnInit{
+
+  id!:number;
+  toCustomer:any
+  user:any
+
+  constructor(private customerService: CustomerService, private route:ActivatedRoute, private router:Router) {}
+  ngOnInit(): void {
+    this.id=this.route.snapshot.params['id']
+    this.customerService.getCustomerById(this.id).subscribe(data=>{
+      this.toCustomer=data;
+    })
+
+  }
 
 }
