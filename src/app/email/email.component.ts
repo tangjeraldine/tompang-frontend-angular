6c93e1aae22e43212e4e18b6628160e56ea7b953
stee-codes <stee.codes@gmail.com>
2023-01-19 16:48:53 +0800

    edited email component

--- src/app/email/email.component.ts
+++ src/app/email/email.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from '../_services/authentication.service';
 import { CustomerService } from '../_services/customer.service';
+import { EmailService } from '../_services/email.service';
 
 @Component({
   selector: 'app-email',
@@ -16,28 +17,28 @@ export class EmailComponent implements OnInit{
   userId!:number;
   user:any
   addNewEmail!: FormGroup;
+  message:any
   
 
-  constructor(private authService:AuthenticationService, private customerService: CustomerService, private route:ActivatedRoute, private router:Router, private addNewEmailFB: FormBuilder,) {}
+  constructor(private authService:AuthenticationService, private emailService: EmailService, private route:ActivatedRoute, private router:Router, private addNewEmailFB: FormBuilder,) {}
   ngOnInit(): void {
     this.toCustomerId=this.route.snapshot.params['id']
-    this.customerService.getCustomerById(this.toCustomerId).subscribe(data=>{
-      this.toCustomer=data;
-    })
+
 
     this.userId=this.authService.generateAuthHeaders()['header_id'];
-    this.customerService.getCustomerById(this.toCustomerId).subscribe(data=>{
-      this.toCustomer=data;
-    })
+
 
     this.addNewEmail = this.addNewEmailFB.group({
-      toEmail: new FormControl(this.toCustomer.email),
-      message: new FormControl(''),
+      message: new FormControl('',Validators.required),
     });
   }
 
   sendEmail() {
-    
+    this.emailService.sendEmail(this.toCustomerId,this.addNewEmail.value.message).subscribe(data=>{
+      alert('Email Sent!')
+    })
+
+
   }
 
 }
