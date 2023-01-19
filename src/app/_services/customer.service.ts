32e47dd87e1ba3e131e559d29a1e27b54417bfa2
tangjeraldine <typjera.1127@gmail.com>
2023-01-19 18:07:15 +0800

    Corrected day and time buttons for trip component.

--- src/app/_services/customer.service.ts
+++ src/app/_services/customer.service.ts
@@ -2,19 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CustomerService {
+  private baseURL = 'http://localhost:8080';
 
-  private baseURL="http://localhost:8080/"
+  customers: any;
 
-  customers:any;
+  constructor(private httpClient: HttpClient) {}
 
-  
-
-  constructor(private httpClient:HttpClient) { }
-
-  getCustomerById(id:any) {
+  getCustomerById(id: any) {
     return this.httpClient.get(`${this.baseURL}/getCustomer/${id}`);
   }
 }
